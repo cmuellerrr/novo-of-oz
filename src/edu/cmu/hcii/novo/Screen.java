af0ae9f2ae16cd29a25a4c03b018134c72cc94cf
Chris Mueller <cmuellerrr@gmail.com>
2013-06-19 16:35:36 -0700

    Updated to have replaceable subscreens instead of menus for screens.

--- src/edu/cmu/hcii/novo/Screen.java
+++ src/edu/cmu/hcii/novo/Screen.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package edu.cmu.hcii.novo;
 
 import java.util.ArrayList;
@@ -24,7 +23,8 @@ public class Screen {
 	  private List<String> paths;
 	  private List<PImage> images;
 	  private int index;
-	  private Map<Character, Screen> menus;
+	  private Map<Character, Screen> subScreens;
+	  private Character subScreenIndex;
 	  
 	  /**
 	   * Create a Screen object from the given json object.
@@ -38,6 +38,7 @@ public class Screen {
 	    index = 0;
 	    paths = new ArrayList<String>();
 	    images = new ArrayList<PImage>();
+	    subScreenIndex = null;
 	    
 	    JSON jsonPaths = json.getArray("paths");
 	    for (int i = 0; i < jsonPaths.length(); i++) {
@@ -47,203 +48,101 @@ public class Screen {
 	    }
 	    
 	    try {
-		    menus = new HashMap<Character, Screen>();	    
-		    JSON jsonMenus = json.getArray("menus");
-		    for (int i = 0; i < jsonMenus.length(); i++) {
-		        JSON curMenu = jsonMenus.getJSON(i);
-		        menus.put(curMenu.getString("key").charAt(0), new Screen(curMenu, parent));
+		    subScreens = new HashMap<Character, Screen>();	    
+		    JSON jsonSubScreens = json.getArray("subScreens");
+		    for (int i = 0; i < jsonSubScreens.length(); i++) {
+		        JSON curSubScreen = jsonSubScreens.getJSON(i);
+		        subScreens.put(curSubScreen.getString("key").charAt(0), new Screen(curSubScreen, parent));
 		    }
 	    } catch (RuntimeException e) {
-	    	System.out.println("No menus specified on this screen.");
+	    	System.out.println("No subscreens specified on screen: " + paths.toString());
 	    }
 	  }
 	  
 	  /**
-	   * Draw the current screen image.
+	   * Draw the current screen image.  If a subscreen is active, draw that.
 	   */
 	  public void draw() {
-		  parent.image(images.get(index), 0, 0, OZ.screenW, OZ.screenH);
+		  if (subScreenIndex != null) {
+			  subScreens.get(subScreenIndex).draw();
+		  } else {
+			  parent.image(images.get(index), 0, 0, OZ.screenW, OZ.screenH);
+		  }
 	  }
 	  
 	  /**
-	   * Return if the screen contains a menu which is triggered
-	   * by the given key.
-	   * 
-	   * @param k
-	   * @return
+	   * Handle any cleanup when the user leaves the screen.
+	   * Call this on screen changes.
 	   */
-	  public boolean hasMenu(char key) {
-	    return menus.containsKey(key); 
+	  public void leaveScreen() {
+		  //clean up any pointers
+		  if (subScreenIndex != null) {
+			  subScreens.get(subScreenIndex).leaveScreen();
+			  subScreenIndex = null;
+		  }
 	  }
 	  
-	  /**
-	   * Get the Menu which is triggered by the given key.
-	   * 
-	   * @param key
-	   * @return
-	   */
-	  public Screen getMenu(char key) {
-	    return menus.get(key); 
+	  public boolean handleKeyPressed(char key) {
+		  if (subScreenIndex != null) {
+			  return subScreens.get(subScreenIndex).handleKeyPressed(key);
+		  } else {
+			  if (subScreens.containsKey(key)) {
+				  subScreenIndex = key;
+				  return true;
+			  }
+			  return false;
+		  }	  
 	  }
 
 	  /**
 	   * Scroll down the screen, so just increase the index.
 	   */
 	  public boolean scrollDown() {
-		  if (index < paths.size()-1) {
-			  index++;
-			  return true;
+		  if (subScreenIndex != null) {
+			  return subScreens.get(subScreenIndex).scrollDown();
+		  } else {
+			  if (index < paths.size()-1) {
+				  index++;
+			  	  return true;
+			  }
+			  return false;
 		  }
-		  return false;
 	  }
 	  
 	  /**
 	   * Scroll up the screen, so just decrease the index.
 	   */
 	  public boolean scrollUp() {
-		  if (index > 0) {
-			  index--;
-			  return true;
+		  if (subScreenIndex != null) {
+			  return subScreens.get(subScreenIndex).scrollUp();
+		  } else {
+			  if (index > 0) {
+				  index--;
+			      return true;
+			  }
+			  return false;
 		  }
-		  return false;
 	  }
 	  
 	  /**
 	   * @return the path
 	   */
 	  public String getPath() {
-		return paths.get(index);
+		  if (subScreenIndex != null) {
+			  return subScreens.get(subScreenIndex).getPath();
+		  } else {
+			  return paths.get(index);
+		  }
 	  }
 
 	  /**
 	   * @return the img
 	   */
 	  public PImage getImage() {
-		  return images.get(index);
+		  if (subScreenIndex != null) {
+			  return subScreens.get(subScreenIndex).getImage();
+		  } else {
+			  return images.get(index);
+		  }
 	  }
 }
-=======
-package edu.cmu.hcii.novo;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.json.JSON;
-
-import processing.core.PApplet;
-import processing.core.PImage;
-
-/**
- * A representation of a prototype screen. Just an image and a 
- * map of possible menus and how they are triggered.
- * 
- * @author Chris
- *
- */
-public class Screen {
-	
-	  private PApplet parent;
-	  private List<String> paths;
-	  private List<PImage> images;
-	  private int index;
-	  private Map<Character, Screen> menus;
-	  
-	  /**
-	   * Create a Screen object from the given json object.
-	   * 
-	   * @param json
-	   * @param parent
-	   */
-	  Screen(JSON json, PApplet parent) {
-	    this.parent = parent;
-	    
-	    index = 0;
-	    paths = new ArrayList<String>();
-	    images = new ArrayList<PImage>();
-	    
-	    JSON jsonPaths = json.getArray("paths");
-	    for (int i = 0; i < jsonPaths.length(); i++) {
-	    	String curPath = jsonPaths.getString(i);
-	    	paths.add(curPath);
-	    	images.add(parent.loadImage("images/" + curPath));
-	    }
-	    
-	    try {
-		    menus = new HashMap<Character, Screen>();	    
-		    JSON jsonMenus = json.getArray("menus");
-		    for (int i = 0; i < jsonMenus.length(); i++) {
-		        JSON curMenu = jsonMenus.getJSON(i);
-		        menus.put(curMenu.getString("key").charAt(0), new Screen(curMenu, parent));
-		    }
-	    } catch (RuntimeException e) {
-	    	System.out.println("No menus specified on this screen.");
-	    }
-	  }
-	  
-	  /**
-	   * Draw the current screen image.
-	   */
-	  public void draw() {
-		  parent.image(images.get(index), 0, 0, OZ.screenW, OZ.screenH);
-	  }
-	  
-	  /**
-	   * Return if the screen contains a menu which is triggered
-	   * by the given key.
-	   * 
-	   * @param k
-	   * @return
-	   */
-	  public boolean hasMenu(char key) {
-	    return menus.containsKey(key); 
-	  }
-	  
-	  /**
-	   * Get the Menu which is triggered by the given key.
-	   * 
-	   * @param key
-	   * @return
-	   */
-	  public Screen getMenu(char key) {
-	    return menus.get(key); 
-	  }
-
-	  /**
-	   * Scroll down the screen, so just increase the index.
-	   */
-	  public boolean scrollDown() {
-		  if (index < paths.size()-1) {
-			  index++;
-			  return true;
-		  }
-		  return false;
-	  }
-	  
-	  /**
-	   * Scroll up the screen, so just decrease the index.
-	   */
-	  public boolean scrollUp() {
-		  if (index > 0) {
-			  index--;
-			  return true;
-		  }
-		  return false;
-	  }
-	  
-	  /**
-	   * @return the path
-	   */
-	  public String getPath() {
-		return paths.get(index);
-	  }
-
-	  /**
-	   * @return the img
-	   */
-	  public PImage getImage() {
-		  return images.get(index);
-	  }
-}
->>>>>>> refs/remotes/origin/master
